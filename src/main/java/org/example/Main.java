d844f3aa1c3efcad3b0cc4ecbde83ef0cdabb97d
Jack Ninway <jack.ninway@gmail.com>
2022-06-28 13:36:42 +0200

    Testing unit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 19;
+        isAdult(age);
+    }
+    public static boolean isAdult(int age){
+        if (age >= 18)
+            return true;
+        else
+            return false;
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,2 +1,26 @@
-package org.example;public class MainTest {
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import org.testng.annotations.Test;
+
+public class MainTest {
+    @Test
+    public void test_isAdult_true(){
+        int age = 18;
+        boolean isAdult = Main.isAdult(age);
+
+        Assertions.assertTrue(isAdult);
+    }
+    @Test
+    public void test_isAdult_false(){
+        //Arrange - Prepare values in order to preform test.
+        int age = 11;
+
+        //Act - Calling the method we intend to test.
+        boolean actualValue = Main.isAdult(age);
+
+        //Assert - Making sure, verify the value as expected.
+        Assertions.assertFalse(actualValue);
+    }
 }
